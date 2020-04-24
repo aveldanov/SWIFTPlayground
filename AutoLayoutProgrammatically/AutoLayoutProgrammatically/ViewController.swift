1243dcb5cc48c74b3c1efe06dfe4180ded92841a
Anton Veldanov <anton.veldanov@gmail.com>
2020-04-24 13:07:09 -0700

    iOS

--- AutoLayoutProgrammatically/AutoLayoutProgrammatically/ViewController.swift
+++ AutoLayoutProgrammatically/AutoLayoutProgrammatically/ViewController.swift
@@ -35,12 +35,16 @@ class ViewController: UIViewController {
       bottom: nil,
       trailing: view.safeAreaLayoutGuide.trailingAnchor,
       padding: .init(top: 0, left: 0, bottom: 0, right: 12),
-      size: .init(width: 100, height: 0) )
+      size: .init(width: 150, height: 0) )
     
     redView.heightAnchor.constraint(equalTo: redView.widthAnchor).isActive = true
     
     
-    blueView.anchor(top: redView.bottomAnchor, leading: nil, bottom: nil, trailing: redView.trailingAnchor, padding: .init(top: 12, left: 0, bottom: 0, right: 0), size: .init(width: 100, height: 100))
+    blueView.anchor(top: redView.bottomAnchor, leading: nil, bottom: nil, trailing: redView.trailingAnchor, padding: .init(top: 12, left: 0, bottom: 0, right: 0))
+    
+    blueView.widthAnchor.constraint(equalTo: redView.widthAnchor).isActive = true
+    blueView.heightAnchor.constraint(equalTo: blueView.widthAnchor).isActive = true
+
     
     
     greenView.anchor(top: redView.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: blueView.bottomAnchor, trailing: redView.leadingAnchor, padding: .init(top: 0, left: 12, bottom: 0, right: 12))
