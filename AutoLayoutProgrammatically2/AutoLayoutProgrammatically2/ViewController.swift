dae05616f3a50639f76406e20713cfcd9e3fe86b
Anton Veldanov <anton.veldanov@gmail.com>
2020-04-24 14:46:26 -0700

    iOS

--- AutoLayoutProgrammatically2/AutoLayoutProgrammatically2/ViewController.swift
+++ AutoLayoutProgrammatically2/AutoLayoutProgrammatically2/ViewController.swift
@@ -10,12 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+  // {} - closure
+  let imagePiece: UIImageView = {
+    let imageView = UIImageView(image: #imageLiteral(resourceName: "image"))
+
+    return imageView
+    
+  }()
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    let imageView = UIImageView(image: #imageLiteral(resourceName: "image"))
-    view.addSubview(imageView)
-    imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+    view.addSubview(imagePiece)
+//    imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+    
+    // enables autolayout
+    imagePiece.translatesAutoresizingMaskIntoConstraints = false
+    
+    imagePiece.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//    imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    imagePiece.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+    
+    imagePiece.widthAnchor.constraint(equalToConstant: 100).isActive = true
+    imagePiece.heightAnchor.constraint(equalToConstant: 100).isActive = true
+
   }
 
 
