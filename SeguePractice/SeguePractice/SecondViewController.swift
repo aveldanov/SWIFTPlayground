c91b086a00dd2eab4353f33e2c227cecf9830839
Anton Veldanov <anton.veldanov@gmail.com>
2020-04-30 18:30:00 -0700

    iOS

---
+++ SeguePractice/SeguePractice/SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  SeguePractice
+//
+//  Created by Veldanov, Anton on 4/30/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  
+  var userName = "Anton"
+  
+  var actRow = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      print(actRow)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SeguePractice/SeguePractice/ViewController.swift
+++ SeguePractice/SeguePractice/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+  var activeRow = 0
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
-
+// pass a vairable to secondView
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "toSecondViewController"{
+      
+      let secondVC = segue.destination as! SecondViewController
+      
+      secondVC.actRow = activeRow    }
+  }
+  
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 4
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+    
+    cell.textLabel?.text = "Row \(indexPath.row)"
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    activeRow = indexPath.row
+    performSegue(withIdentifier: "toSecondViewController", sender: nil)
+  }
+  
+  
+  
 }
 
