c0b8dead4f998eff4801a73612733104b76f51b3
Anton Veldanov <anton.veldanov@gmail.com>
2020-05-02 18:38:18 -0700

    iOS

--- APIsPractice/APIsPractice/ViewController.swift
+++ APIsPractice/APIsPractice/ViewController.swift
@@ -7,14 +7,64 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var resultLabel: UILabel!
+  
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
   }
-
-
+  
+  @IBAction func submitButtonPressed(_ sender: UIButton) {
+    // removing spaces
+    if let url = URL(string: "https://samples.openweathermap.org/data/2.5/weather?q=" + textField.text!.replacingOccurrences(of: " ", with: "%20") + ",uk&appid=7636ee5a3761e06a869b130e641380e9"){
+      
+      let session = URLSession.shared
+      
+      let task = session.dataTask(with: url) { (data, response, error) in
+        
+        if error != nil{
+          
+          print(error)
+        }else{
+          if let safeData = data{
+            do{
+              
+              let json = try JSON(data: safeData)
+              
+              let description = json["weather"][0]["description"].stringValue
+              
+              DispatchQueue.main.async {
+                self.resultLabel.text = description
+              }
+              
+              
+              
+            }catch{
+              
+              print("JSON parsing Error")
+              
+            }
+            
+            
+          }
+          
+        }
+        
+      }
+      task.resume()
+      
+      
+      
+    }
+    
+  }
+  
 }
 
