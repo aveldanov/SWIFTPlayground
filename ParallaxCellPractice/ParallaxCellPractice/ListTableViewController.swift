7ffd12a1140a3792d2e5b27f2174531c303d2175
Anton Veldanov <anton.veldanov@gmail.com>
2020-05-22 14:28:21 -0700

    iOS

---
+++ ParallaxCellPractice/ParallaxCellPractice/ListTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ListTableViewController.swift
+//  ParallaxCellPractice
+//
+//  Created by Veldanov, Anton on 5/22/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
