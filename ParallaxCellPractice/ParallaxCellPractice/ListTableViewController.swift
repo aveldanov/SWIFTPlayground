3fe2f7ed856f2949c0fc24088f1fcd4b509f2ade
Anton Veldanov <anton.veldanov@gmail.com>
2020-05-22 23:50:22 -0700

    iOS

--- ParallaxCellPractice/ParallaxCellPractice/ListTableViewController.swift
+++ ParallaxCellPractice/ParallaxCellPractice/ListTableViewController.swift
@@ -12,9 +12,23 @@ class ListTableViewController: UITableViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+  
+  
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    1
   }
 
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 4
+  }
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    <#code#>
+  }
+  
 
 }
 
