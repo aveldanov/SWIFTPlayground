e28839098b60fd888990eac4c58c28643c0f634b
Anton Veldanov <anton.veldanov@gmail.com>
2020-05-23 00:19:31 -0700

    iOS

---
+++ ParallaxCellPractice/ParallaxCellPractice/View/ParallaxViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  ParallaxViewCell.swift
+//  ParallaxCellPractice
+//
+//  Created by Veldanov, Anton on 5/22/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import UIKit
+
+class ParallaxViewCell: UITableViewCell {
+
+  @IBOutlet weak var itemImageView: UIImageView!
+  
+  @IBOutlet weak var descLabel: UILabel!
+  
+  override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+  func configureCell(withImage image:UIImage, andDescription desc:String){
+    
+    itemImageView.image = image
+    descLabel.text = desc
+  }
+  
+  
+  func setupParallax(){
+    
+    
+    
+  }
+
+}
