6b7285072beb6ff50ca4a06e0b93bdb428d5d47b
Anton Veldanov <anton.veldanov@gmail.com>
2020-05-23 00:21:13 -0700

    iOS

---
+++ ParallaxCellPractice/ParallaxCellPractice/ImagesNames.swift
@@ -0,0 +1,19 @@
+//
+//  ImagesNames.swift
+//  ParallaxCellPractice
+//
+//  Created by Veldanov, Anton on 5/23/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import UIKit
+
+
+var camera = UIImage(named: "1")
+var city = UIImage(named: "2")
+var camera = UIImage(named: "3")
+var camera = UIImage(named: "4")
+var camera = UIImage(named: "5")
+var camera = UIImage(named: "6")
+
+
