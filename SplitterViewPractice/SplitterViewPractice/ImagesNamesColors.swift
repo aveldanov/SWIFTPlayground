13e00f6ab28b7a2ed17687d039818e9e38721f02
Anton Veldanov <anton.veldanov@gmail.com>
2020-05-23 21:39:26 -0700

    iOS

--- SplitterViewPractice/SplitterViewPractice/ImagesNamesColors.swift
+++ SplitterViewPractice/SplitterViewPractice/ImagesNamesColors.swift
@@ -16,6 +16,10 @@ let  flowers = UIImage(named: "4")!
 let  stand = UIImage(named: "5")!
 let  urban = UIImage(named: "6")!
 
+let imageArray = [camera,city, buffalo,flowers,stand,urban]
 
+var namesArray = ["CAMERA","CITY","ANIMALS","FLOWERS","STANDS","URBAN" ]
+
+let colorsArray = [#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1),#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1),#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1),#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)]
 
 
