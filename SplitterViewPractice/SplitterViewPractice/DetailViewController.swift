6240a0793da7dd178b7e4b1eef44cab47253c39a
Anton Veldanov <anton.veldanov@gmail.com>
2020-05-23 21:53:41 -0700

    iOS

--- SplitterViewPractice/SplitterViewPractice/DetailViewController.swift
+++ SplitterViewPractice/SplitterViewPractice/DetailViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-
+  @IBOutlet weak var itemImageView: UIImageView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
