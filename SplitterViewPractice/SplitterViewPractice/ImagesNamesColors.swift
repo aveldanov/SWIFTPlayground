a271495e08051af76d4661c7f9980ca6cb739d14
Anton Veldanov <anton.veldanov@gmail.com>
2020-05-23 21:05:58 -0700

    iOS

---
+++ SplitterViewPractice/SplitterViewPractice/ImagesNamesColors.swift
@@ -0,0 +1,9 @@
+//
+//  ImagesNamesColors.swift
+//  SplitterViewPractice
+//
+//  Created by Veldanov, Anton on 5/23/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import Foundation
