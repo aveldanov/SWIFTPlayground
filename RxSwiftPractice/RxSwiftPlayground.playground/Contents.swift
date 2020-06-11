29c16c6e5d28fbb83ed6390edb31b0fb8dde9d66
Anton Veldanov <anton.veldanov@gmail.com>
2020-06-10 22:06:56 -0700

    iOS

--- RxSwiftPractice/RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPractice/RxSwiftPlayground.playground/Contents.swift
@@ -6,3 +6,22 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 var observable: Observable<String>
 
 
+let numberSequence = Observable.just(5)
+let numberSubscription = numberSequence.subscribe(onNext:{
+  print($0)
+})
+
+
+let helloSequence = Observable.from(["H","e","l","l","o"])
+
+
+let helloSubscription = helloSequence.subscribe { (event) in
+  switch event {
+  case .next(let value):
+    print(value)
+  case .error(let error):
+    print(error)
+  case .completed:
+    print("Completed")
+  }
+}
