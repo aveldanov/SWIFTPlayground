145d884164260e02124fb68fa57c7634701ba5ce
Anton Veldanov <anton.veldanov@gmail.com>
2020-06-15 23:08:15 -0700

    iOS

--- RxSwiftPractice/RxSwiftPlayground.playground/Pages/BehaivorSubject.xcplaygroundpage/Contents.swift
+++ RxSwiftPractice/RxSwiftPlayground.playground/Pages/BehaivorSubject.xcplaygroundpage/Contents.swift
@@ -2,4 +2,6 @@ import RxSwift
 import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
+// GIVES MOST RECENT SUBJECT AND EVERYTHING AFTER
+
 var behaivorSubject = BehaviorSubject
