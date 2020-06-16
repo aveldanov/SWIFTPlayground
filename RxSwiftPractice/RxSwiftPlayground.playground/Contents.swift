b487257303d6018c98dabce1ea979b68f6e58707
Anton Veldanov <anton.veldanov@gmail.com>
2020-06-15 22:32:37 -0700

    iOS

--- RxSwiftPractice/RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPractice/RxSwiftPlayground.playground/Contents.swift
@@ -4,13 +4,13 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 
 
 //var observable: Observable<String>
-
+let disposeBag = DisposeBag()
 
 let numberSequence = Observable.just(5)
 
 let numberSubscription = numberSequence.subscribe(onNext: {
   print($0)
-})
+}).disposed(by: disposeBag)
 
 
 let helloSequence = Observable.from(["H","e","l","l","o"])
@@ -26,10 +26,14 @@ let helloSubscription = helloSequence.subscribe { (event) in
       print("Completed")
 
   }
-}
+}.disposed(by: disposeBag)
 
 
 // Disposing
+//helloSubscription.dispose() // free up memory -> explicit call not recommended -> use DisposeBag
+
+
+
 
 
 
